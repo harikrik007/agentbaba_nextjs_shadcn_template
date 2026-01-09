4078380caf0082e46ef5dada2ffdeacd1d4380a0
harikrik007 <iamharihk@gmail.com>
2026-01-10 04:06:30 +0530

    minor change next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 }
 
