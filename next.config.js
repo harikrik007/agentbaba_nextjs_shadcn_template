5dab00b1c075b946e883f1dd06501129b008d6c0
harikrik007 <iamharihk@gmail.com>
2026-01-15 20:37:27 +0530

    added unoptimized true to reduce cpu usage in development

--- next.config.js
+++ next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
         hostname: '**',
       },
     ],
+    // DISABLE optimization in development to save CPU
+    unoptimized: process.env.NODE_ENV === 'development',
   },
 }
 
